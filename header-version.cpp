9e09da3f521e91f73557f79c2e02d27ba2ad2548
Matthew Milano <milano@cs.cornell.edu>
2019-08-28 12:00:17 -0400

    a more realistic header version reveals some shortcomings

--- header-version.cpp
+++ header-version.cpp
@@ -8,17 +8,32 @@
 #include <memory>
 #include <type_traits>
 
+struct buffer_maker {
+  char *make_buffer(std::size_t size) { return (char *)malloc(size); }
+  void free_buffer(void *v) { ::free(v); }
+};
+
 template <std::size_t header_size> struct allocator {
 
+  buffer_maker &bm;
   struct _i {
 
-    char *serial_region{0};
+    using char_p = char *;
+    const char_p serial_region;
     std::size_t serial_offset{header_size};
     const std::size_t serial_size;
-    std::map<void const *const, std::function<char *()>> allocated_serializers;
+    std::map<void *,
+             std::pair<std::function<char *()>, std::function<void(void *)>>>
+        allocated_serializers;
 
     _i(void *serial_region, std::size_t serial_size)
         : serial_region((char *)serial_region), serial_size(serial_size) {}
+    ~_i() {
+      for (const auto &p : allocated_serializers) {
+        p.second.second(p.first);
+        ::free(p.first);
+      }
+    }
 
     void *alloc_serial(std::size_t chunk_size) {
       void *ret = (((char *)serial_region) + serial_offset);
@@ -37,11 +52,14 @@ template <std::size_t header_size> struct allocator {
 
     template <typename T> T *dyn_alloc() {
       auto *ret = (T *)malloc(sizeof(T));
-      allocated_serializers[ret] = [ret, this] {
-        char *obj_loc = serial_region + serial_offset;
-        serial_offset += mutils::to_bytes(*ret, obj_loc);
-        return obj_loc;
-      };
+      allocated_serializers[ret] =
+          typename decltype(allocated_serializers)::mapped_type(
+              [ret, this] {
+                char *obj_loc = serial_region + serial_offset;
+                serial_offset += mutils::to_bytes(*ret, obj_loc);
+                return obj_loc;
+              },
+              [](void *call_destructor) { ((T *)call_destructor)->~T(); });
       return ret;
     }
 
@@ -76,8 +94,12 @@ template <std::size_t header_size> struct allocator {
         std::size_t indx = 0;
         for (auto *arg : dynamic_args) {
           if (arg) {
-            assert(allocated_serializers.count(arg) > 0);
-            offsets[indx] = allocated_serializers.at(arg)() - serial_region;
+            // n.b. these const-casts are safe because *at runtime* the arg will
+            // be treated as const by count and at.
+            assert(allocated_serializers.count(const_cast<void *>(arg)) > 0);
+            offsets[indx] =
+                allocated_serializers.at(const_cast<void *>(arg)).first() -
+                serial_region;
           }
           ++indx;
         }
@@ -94,28 +116,38 @@ template <std::size_t header_size> struct allocator {
   };
 
   template <typename... Args> void *prep_send(const Args &... d) {
-    return i->prep_send(d...);
+    auto ret = (*i)->prep_send(d...);
+    i->reset(new _i{bm.make_buffer((*i)->serial_size), (*i)->serial_size});
+    return ret;
   }
 
   template <typename T, typename... U>
   decltype(auto) alloc_and_init(U &&... u) {
-    return i->template alloc_and_init<T>(std::forward<U>(u)...);
+    return (*i)->template alloc_and_init<T>(std::forward<U>(u)...);
   }
 
   template <typename... T> decltype(auto) del(T &&... t) {
-    return i->template del(std::forward<T>(t)...);
+    return (*i)->template del(std::forward<T>(t)...);
   }
 
-  std::shared_ptr<struct _i> i;
-  allocator(void *serial_region, std::size_t serial_size)
-      : i(new _i{serial_region, serial_size}) {}
+  std::shared_ptr<std::unique_ptr<_i>> i;
+  allocator(buffer_maker &bm, std::size_t serial_size)
+      : bm(bm), i(new std::unique_ptr<_i>{
+                    new _i{bm.make_buffer(serial_size), serial_size}}) {}
+
+  ~allocator() {
+    if (*i)
+      bm.free_buffer((*i)->serial_region);
+  }
 };
 
 constexpr const std::size_t size = 1024;
 int main() {
-  std::array<char, size> one;
-  allocator<1024> a{one.data(), sizeof(one)};
+  buffer_maker bm;
+  allocator<1024> a{bm, size};
   auto *i = a.alloc_and_init<std::list<int>>(0);
-  a.prep_send(*i);
   a.del(i);
+  i = a.alloc_and_init<std::list<int>>(0);
+  auto sndbuf = a.prep_send(*i);
+  bm.free_buffer(sndbuf);
 }
